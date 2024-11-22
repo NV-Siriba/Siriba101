294dab86208cd9de0a777ae70c43e3bc800ec385
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2024-11-22 11:43:51 +0300

    Add files via upload

---
+++ python_lists.py
@@ -0,0 +1,9 @@
+my_list=[20,30,40]
+my_list.append(10,)
+my_list.insert(1,15)
+#more=[50,60,70]
+my_list.extend([50,60,70])
+#del my_list[-1]
+my_list.remove(70)
+my_list.sort()
+print(my_list.index(30))
